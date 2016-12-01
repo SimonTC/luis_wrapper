be5dca163478d91cf3dbf8ca41a9b47bd460c92e
Simon Clement <simon.clement+git@gmail.com>
2016-12-01 17:59:42 +0200

    Add tests to be fleshed out later

--- tests/test_LuisResponse.py
+++ tests/test_LuisResponse.py
@@ -1,2 +1,16 @@
 import pytest
 from luis_wrapper import LuisResponse
+
+
+
+def test_Given_EssentialParametersAreMissing_When_InitializingClass_Then_ExceptionIsRaised():
+    assert False
+
+def test_Given_NoEssentialParametersAreMissing_When_InitializingClass_Then_ClassIsInitializedCorrectly():
+    assert False
+
+def test_Given_NonEssentialParametersAreMissing_When_InitializingClass_Then_ClassIsInitializedCorrectlyWithMissingAttributesSetToNone():
+    assert False
+
+def test_Given_AllParametersAreGiven_When_InitializingClass_Then_ClassIsInitializedCorrectlyWithoutAnyNoneValues():
+    assert False
