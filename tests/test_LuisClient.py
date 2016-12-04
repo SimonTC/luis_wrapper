06b21a12dea36ad5665f50968b8bcd60ff381065
Simon Clement <simon.clement+git@gmail.com>
2016-12-04 20:28:46 +0200

    Start work on test of Client

---
+++ tests/test_LuisClient.py
@@ -0,0 +1,33 @@
+import pytest
+#import context
+from luis_wrapper.LuisClient import Client
+
+class LuisClientTest:
+
+    @pytest.fixture
+    def client(self):
+        app_id = "An app id"
+        subscription_key = "A subscription key"
+        return Client(app_id, subscription_key)
+
+
+    def test_ParametetersAreSetCorrectly(self):
+        app_id = "An app id"
+        subscription_key = "A subscription key"
+        client = Client(app_id, subscription_key)
+        assert client.app_id == app_id
+        assert client.subscription_key == subscription_key
+
+    @pytest.mark.parametrize("input", [
+        (''), (' '), ('     ')
+    ])
+    def test_Given_EmptyString_When_CallingAnalyze_Then_ExceptionIsRaised(self, client, input):
+        with pytest.raises(ValueError) as err:
+            client.analyze(input)
+
+
+    def test_Given_NoneAsString_When_CallingAnalyze_Then_ExceptionIsRaised(self):
+        assert False
+
+    def test_Given_NonEmptyString_When_CallingAnalyze_Then_ConversationIsReturned(self):
+        assert False
