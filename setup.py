065b41b6a0c0c13ff146cdb792d5fcb992e0760a
Simon Clement <simon.clement+git@gmail.com>
2016-12-06 19:31:25 +0200

    Update setup files

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 setup(
-  name = 'luis_wrapper',
-  packages = ['luis_wrapper'], # this must be the same as the name above
-  version = '0.1.0',
-  description = 'A wrapper around Microsoft LUIS',
-  author = 'Simon Clement',
-  author_email = 'simon.clement@gmail.com',
-  url = 'https://github.com/SimonTC/luis_wrapper', # use the URL to the github repo
-  download_url = 'https://github.com/SimonTC/luis_wrapper/tarball/0.1.0',
-  keywords = ['LUIS', 'NLP'], # arbitrary keywords
-  classifiers = [],
+    name = 'luis_wrapper',
+    packages=find_packages(exclude=['docs', 'tests*']),
+    version = '0.1.0',
+    description = 'A wrapper around Microsoft LUIS',
+    author = 'Simon Clement',
+    author_email = 'simon.clement@gmail.com',
+    url = 'https://github.com/SimonTC/luis_wrapper',
+    download_url = 'https://github.com/SimonTC/luis_wrapper/tarball/0.1.0',
+    keywords = ['LUIS', 'NLP'],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+    ],
+    install_requires=['requests'],
+    license='MIT'
 )
