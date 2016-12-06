29901addfeda99af29c1d816e99131320f6084b2
Simon Clement <simon.clement+git@gmail.com>
2016-12-06 19:05:59 +0200

    Prepare setup.py for PyPi

--- setup.py
+++ setup.py
@@ -1,2 +1,13 @@
-from setuptools import setup
-setup(name='Luis Wrapper', packages=['luis_wrapper'])
+from distutils.core import setup
+setup(
+  name = 'luis_wrapper',
+  packages = ['luis_wrapper'], # this must be the same as the name above
+  version = '0.1.0',
+  description = 'A wrapper around Microsoft LUIS',
+  author = 'Simon Clement',
+  author_email = 'simon.clement@gmail.com',
+  url = 'https://github.com/SimonTC/luis_wrapper', # use the URL to the github repo
+  download_url = 'https://github.com/SimonTC/luis_wrapper/tarball/0.1.0',
+  keywords = ['LUIS', 'NLP'], # arbitrary keywords
+  classifiers = [],
+)
